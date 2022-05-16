7bbf2f2c8014f32bc26558df94ab2661fe5cbd62
Jerry Pierre <jpier132@fiu.edu>
2022-05-16 18:18:40 -0400

    develops first commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import './App.scss';
+import HomePage from './Pages/HomePage/HomePage';
+import LandingPage from './Pages/LandingPage/LandingPage';
+import SignUpPage from './Pages/SignUpPage/SignUpPage';
+import SignInPage from './Pages/SignInPage/SignInPage';
 
 
 function App() {
@@ -9,7 +13,8 @@ function App() {
         <Switch>
           <Route path="/" component={HomePage} />
           <Route path="/about" component={LandingPage} />
-          {/* <Route path="/contact" component={Contact} /> */}
+          <Route path="/register" component={SignUpPage} />
+          <Route path="/login" component={SignInPage} />
         </Switch>
       </BrowserRouter>
      
